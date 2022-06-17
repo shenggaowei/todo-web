74b06473255494e565acb50ba5c3141806cc6137
weishenggao <527518190@qq.com>
2022-06-17 19:02:18 +0800

    添加 todo 初始版本，体验一把 setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     Components({
-    resolvers: [AntDesignVueResolver()]
-  })]
+      resolvers: [AntDesignVueResolver()],
+    }),
+    vueJsx({})
+  ]
 })
