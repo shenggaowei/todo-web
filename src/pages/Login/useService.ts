de6ebc67589e2e49f9ce66ff2d3d996e580819a1
weishenggao <527518190@qq.com>
2022-08-16 19:20:13 +0800

    format code

--- src/pages/Login/useService.ts
+++ src/pages/Login/useService.ts
@@ -1,22 +1,22 @@
 import { useAsyncState } from "@vueuse/core";
 import { signIn } from "@/service/auth";
-import type { ISignInParams  } from '@/service/auth/interface'
+import type { ISignInParams } from '@/service/auth/interface'
 
 type TUseSignInParams = Pick<ISignInParams, 'password' | 'userName'>
 
 export function useSignIn(params: TUseSignInParams) {
     const { state, execute, isLoading } = useAsyncState(() => {
-      return signIn({
-          ...params,
-          origin: 'pc'
-      })
+        return signIn({
+            ...params,
+            origin: 'pc'
+        })
     },
-    {
-        token: ''
+        {
+            token: ''
         },
         {
-        immediate: false
-    })
+            immediate: false
+        })
     return {
         state,
         execute,
